c041e775f504eb5fbb2a991e25a8e7e2555648bc
leo.zhu <ace.leo.zhu@gmail.com>
2017-12-10 23:00:06 +0800

    auto_push_commit

---
+++ tensorflow/008-simple_regression.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+tf.set_random_seed(1)
+np.random.seed(1)
+
+#fake data
+x = np.linspace(-1, 1, 300)[:, np.newaxis]
+y = np.power(x, 2) + np.random.normal(0, 0.05, x.shape)
+
+#placeholder
+xs = tf.placeholder(tf.float32, x.shape)
+ys = tf.placeholder(tf.float32, y.shape)
+
+#create layer
+hidden = tf.layers.dense(xs, 10, tf.nn.relu)
+output = tf.layers.dense(hidden, 1)
+
+#create loss
+loss = tf.losses.mean_squared_error(ys, output)
+train = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(1000):
+    sess.run(train, feed_dict={xs: x, ys: y})
+
+    if step % 50 == 0:
+        print(sess.run(loss, feed_dict={xs: x, ys: y}))
+
+        plt.cla()
+        plt.scatter(x, y, s=10, c=x, cmap='rainbow')
+        plt.plot(x, sess.run(output, feed_dict={xs: x, ys: y}), 'r-', lw=1)
+        plt.pause(0.1)
+
+plt.pause(3)
