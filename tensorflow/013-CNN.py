b8aadbf8c8e2911ef2db20cfe85d91776a6e3cef
leo.zhu <ace.leo.zhu@gmail.com>
2017-12-18 00:13:53 +0800

    auto_push_commit

--- tensorflow/013-CNN.py
+++ tensorflow/013-CNN.py
@@ -1,63 +1,38 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
 
-# number 1 to 10 data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+BATCH_SIZE = 50
+BATCH_SIZE_TEST = 200
 
+tf.set_random_seed(1)
+np.random.seed(1)
 
-def compute_accuracy(v_xs, v_ys):
-    global prediction
-    y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
-    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
-    return result
+mnist = input_data.read_data_sets('mnist', one_hot=True)
 
+test_x = mnist.test.images[:BATCH_SIZE_TEST]
+test_y = mnist.test.labels[:BATCH_SIZE_TEST]
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    # stride [1, x_movement, y_movement, 1]
-    # Must have strides[0] = strides[3] = 1
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-    # stride [1, x_movement, y_movement, 1]
-    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
-
-# define placeholder for inputs to network
-xs = tf.placeholder(tf.float32, [None, 784]) # 28x28
+xs = tf.placeholder(tf.float32, [None, 784]) # 28*28
+image = tf.reshape(xs, [-1, 28, 28, 1])
 ys = tf.placeholder(tf.float32, [None, 10])
-keep_prob = tf.placeholder(tf.float32)
 
-## conv1 layer ##
- 
-## conv2 layer ##
+con1 = tf.layers.conv2d(image, 16, 5, 1, 'same', activation=tf.nn.relu) # 28*28*16
+p1 = tf.layers.max_pooling2d(con1, 2, 2) # 14*14*16
+con2 = tf.layers.conv2d(p1, 32, 5, 1, 'same', activation=tf.nn.relu) #14*14*32
+p2 = tf.layers.max_pooling2d(con2, 2, 2) #7*7*32
+output = tf.layers.dense(tf.reshape(p2, [-1, 7*7*32]), 10) # => (7*7*32, )
 
-## func1 layer ##
+loss = tf.losses.softmax_cross_entropy(onehot_labels=ys, logits=output)
+train = tf.train.AdamOptimizer(0.1).minimize(loss)
 
-## func2 layer ##
-
-
-# the error between prediction and real data
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), axis=1))       # loss
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
-sess = tf.Session()
+sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
-for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
-    if i % 50 == 0:
-        print(compute_accuracy(
-            mnist.test.images[:1000], mnist.test.labels[:1000]))
+for step in range(101):
+    b_x, b_y = mnist.train.next_batch(BATCH_SIZE)
+    sess.run([train], feed_dict={xs: b_x, ys: b_y})
+
+    if step % 50 == 0:
+        l, op = sess.run([loss, output], feed_dict={xs: test_x, ys: test_y})
+        print(l, np.argmax(op, 1))
--- tensorflow_tutorial-contents/401_CNN.py
+++ tensorflow_tutorial-contents/401_CNN.py
@@ -15,27 +15,32 @@ import matplotlib.pyplot as plt
 tf.set_random_seed(1)
 np.random.seed(1)
 
+BATCH_SIZE_TEST = 200
 BATCH_SIZE = 50
 LR = 0.001              # learning rate
 
-mnist = input_data.read_data_sets('./mnist', one_hot=True)  # they has been normalized to range (0,1)
-test_x = mnist.test.images[:2000]
-test_y = mnist.test.labels[:2000]
+# they has been normalized to range (0,1), one_hot 非0即1
+mnist = input_data.read_data_sets('./mnist', one_hot=True)
+test_x = mnist.test.images[:BATCH_SIZE_TEST]
+test_y = mnist.test.labels[:BATCH_SIZE_TEST]
 
 # plot one example
-print(mnist.train.images.shape)     # (55000, 28 * 28)
-print(mnist.train.labels.shape)   # (55000, 10)
-plt.imshow(mnist.train.images[0].reshape((28, 28)), cmap='gray')
-plt.title('%i' % np.argmax(mnist.train.labels[0])); plt.show()
+# print(mnist.train.images.shape)     # (55000, 28 * 28)
+# print(mnist.train.labels.shape)   # (55000, 10)
+# plt.imshow(mnist.train.images[0].reshape((28, 28)), cmap='gray')
+# plt.title('%i' % np.argmax(mnist.train.labels[0])); plt.show()
 
-tf_x = tf.placeholder(tf.float32, [None, 28*28]) / 255.
+# (50, 784) (50, 10)
+#这里除以255没有意义，因为输入的数据都已经经过one_hot处理，非0即1
+# tf_x = tf.placeholder(tf.float32, [None, 28*28]) / 255.
+tf_x = tf.placeholder(tf.float32, [None, 28*28])
 image = tf.reshape(tf_x, [-1, 28, 28, 1])              # (batch, height, width, channel)
 tf_y = tf.placeholder(tf.int32, [None, 10])            # input y
 
 # CNN
 conv1 = tf.layers.conv2d(   # shape (28, 28, 1)
     inputs=image,
-    filters=16,
+    filters=16,  #这里的输出是任意的
     kernel_size=5,
     strides=1,
     padding='same',
@@ -63,28 +68,28 @@ sess.run(init_op)     # initialize var in graph
 
 # following function (plot_with_labels) is for visualization, can be ignored if not interested
 from matplotlib import cm
-try: from sklearn.manifold import TSNE; HAS_SK = True
-except: HAS_SK = False; print('\nPlease install sklearn for layer visualization\n')
-def plot_with_labels(lowDWeights, labels):
-    plt.cla(); X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
-    for x, y, s in zip(X, Y, labels):
-        c = cm.rainbow(int(255 * s / 9)); plt.text(x, y, s, backgroundcolor=c, fontsize=9)
-    plt.xlim(X.min(), X.max()); plt.ylim(Y.min(), Y.max()); plt.title('Visualize last layer'); plt.show(); plt.pause(0.01)
+# try: from sklearn.manifold import TSNE; HAS_SK = True
+# except: HAS_SK = False; print('\nPlease install sklearn for layer visualization\n')
+# def plot_with_labels(lowDWeights, labels):
+#     plt.cla(); X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
+#     for x, y, s in zip(X, Y, labels):
+#         c = cm.rainbow(int(255 * s / 9)); plt.text(x, y, s, backgroundcolor=c, fontsize=9)
+#     plt.xlim(X.min(), X.max()); plt.ylim(Y.min(), Y.max()); plt.title('Visualize last layer'); plt.show(); plt.pause(0.01)
 
-plt.ion()
-for step in range(600):
+# plt.ion()
+for step in range(101):
     b_x, b_y = mnist.train.next_batch(BATCH_SIZE)
     _, loss_ = sess.run([train_op, loss], {tf_x: b_x, tf_y: b_y})
     if step % 50 == 0:
         accuracy_, flat_representation = sess.run([accuracy, flat], {tf_x: test_x, tf_y: test_y})
         print('Step:', step, '| train loss: %.4f' % loss_, '| test accuracy: %.2f' % accuracy_)
 
-        if HAS_SK:
-            # Visualization of trained flatten layer (T-SNE)
-            tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000); plot_only = 500
-            low_dim_embs = tsne.fit_transform(flat_representation[:plot_only, :])
-            labels = np.argmax(test_y, axis=1)[:plot_only]; plot_with_labels(low_dim_embs, labels)
-plt.ioff()
+        # if HAS_SK:
+        #     # Visualization of trained flatten layer (T-SNE)
+        #     tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000); plot_only = 500
+        #     low_dim_embs = tsne.fit_transform(flat_representation[:plot_only, :])
+        #     labels = np.argmax(test_y, axis=1)[:plot_only]; plot_with_labels(low_dim_embs, labels)
+# plt.ioff()
 
 # print 10 predictions from test data
 test_output = sess.run(output, {tf_x: test_x[:10]})
