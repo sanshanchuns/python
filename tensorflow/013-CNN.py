dd4f89d8025d7d6915adc211d5bfdda2c6ff25df
leo <zhu_lia@Ctrip.com>
2017-12-18 18:44:21 +0800

    auto_push_commit

--- tensorflow/013-CNN.py
+++ tensorflow/013-CNN.py
@@ -3,7 +3,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 
 BATCH_SIZE = 50
-BATCH_SIZE_TEST = 200
+BATCH_SIZE_TEST = 10
+LR = 0.01
 
 tf.set_random_seed(1)
 np.random.seed(1)
@@ -24,15 +25,16 @@ p2 = tf.layers.max_pooling2d(con2, 2, 2) #7*7*32
 output = tf.layers.dense(tf.reshape(p2, [-1, 7*7*32]), 10) # => (7*7*32, )
 
 loss = tf.losses.softmax_cross_entropy(onehot_labels=ys, logits=output)
-train = tf.train.AdamOptimizer(0.1).minimize(loss)
+train = tf.train.AdamOptimizer(LR).minimize(loss)
 
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
-for step in range(101):
+for step in range(1001):
     b_x, b_y = mnist.train.next_batch(BATCH_SIZE)
     sess.run([train], feed_dict={xs: b_x, ys: b_y})
 
     if step % 50 == 0:
         l, op = sess.run([loss, output], feed_dict={xs: test_x, ys: test_y})
+        print(np.argmax(test_y, 1))
         print(l, np.argmax(op, 1))
