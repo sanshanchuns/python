0a7f7b3314a33a8d7d9d587c1de1170afb68ef10
leo <zhu_lia@Ctrip.com>
2017-12-28 18:00:00 +0800

    auto_push_commit

--- tensorflow/000-demo.py
+++ tensorflow/000-demo.py
@@ -1,25 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.random.rand(2, 2, 2)
-# r, g, b = x[:,:,0], x[:,:,1], x[:,:,2]
-# gray = 0.29*r + 0.58*g + 0.11*b
-# gray = np.mean(x, -1)
-# print(x)
-# print(gray)
-# print(x.flatten())
-import tensorflow as tf
 
-steps = np.linspace(0, np.pi*2, 100, dtype=np.float32)
-# use sin predicts cos
-x = np.sin(steps)    # shape (batch, time_step, input_size)
-y = np.cos(steps)
-z = np.tan(steps)
+a = np.array((1,2,3))
+b = np.array((2,3,4))
+c = np.hstack((a,b))
 
-plt.plot(x)
-plt.plot(y)
-plt.plot(z)
-plt.show()
+print(c)
+
+a = np.array([[1],[2],[3]])
+b = np.array([[2],[3],[4]])
+d = np.hstack((a,b))
+
+print(d)
+
 
 
 
