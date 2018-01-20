3d21d8e48885694e1dd8df2abf7c6f6774eecce4
leo.zhu <ace.leo.zhu@gmail.com>
2018-01-20 23:02:00 +0800

    leodeMacBook-Air.local Sat Jan 20 23:02:00 CST 2018

--- reinforcement_learning/2_Q_Learning_maze/RL_brain.py
+++ reinforcement_learning/2_Q_Learning_maze/RL_brain.py
@@ -24,7 +24,7 @@ class QLearningTable:
             # choose best action
             state_action = self.q_table.loc[observation, :]
             state_action = state_action.reindex(np.random.permutation(state_action.index))     # some actions have same value
-            action = state_action.idxmax()
+            action = state_action.idxmax() #比较值大小返回大值所在的索引
         else:
             # choose random action
             action = np.random.choice(self.actions)
