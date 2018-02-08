565bfbe28dfe307b3b77276f85705d923903b8a9
leo <zhu_lia@Ctrip.com>
2018-02-08 18:00:00 +0800

    leodeMacBook-Pro-2.local Thu Feb  8 18:00:00 CST 2018

--- PyTorchZeroToAll/03__auto_gradient.py
+++ PyTorchZeroToAll/03__auto_gradient.py
@@ -3,12 +3,10 @@ import torch
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
-x_data = [1, 2, 3]
-y_data = [1, 4, 9]
+x_data = [1.0, 2.0, 3.0]
+y_data = [1.0, 4.0, 9.0]
 
 w = Variable(torch.Tensor([1.0]), requires_grad=True)
-w_his = []
-loss_his = []
 
 def forward(x):
     return w*x
@@ -17,18 +15,12 @@ def loss(x, y):
     y_pred = forward(x)
     return (y_pred - y)*(y_pred - y)
 
-for epoch in range(100):
+for epoch in range(10):
     for x, y in zip(x_data, y_data):
         l = loss(x, y)
         l.backward()
-
         w.data = w.data - 0.01*w.grad.data
-
         w.grad.data.zero_()
 
-print(w.data)
-
-plt.plot(w_his, loss_his)
-plt.show()
-
+    print("process: ", epoch, l.data[0])
 
