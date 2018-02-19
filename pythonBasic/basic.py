d4a361c769c7c2b847b7375fcef8220b87771caa
leo.zhu <ace.leo.zhu@gmail.com>
2018-02-20 03:42:54 +0800

    leodeMacBook-Air.local Tue Feb 20 03:42:54 CST 2018

--- pythonBasic/basic.py
+++ pythonBasic/basic.py
@@ -1,50 +1,7 @@
-for i in range(10):
-    print('{}'.format(i), end='')
-print()
+l = [
+    ['test', 'hello', 'world', 'sm', 'philpbin', 'english'],
+    ['test2', 'hello2', 'world2', 'sm2', 'philpbin2', 'english2'],
+    ]
 
-colors = ['red', 'green', 'blue', 'yellow']
-
-for color in colors:
-    print('{} '.format(color), end='')
-print()
-
-for color in reversed(colors):
-    print('%s ' % color, end='')
-print()
-
-for i, color in enumerate(colors):
-    print(i, color)
-
-names = ['raymod', 'rachel', 'matthew']
-for name, color in zip(names, colors):
-    print(name, color)
-
-for color in sorted(colors):
-    print('{} '.format(color), end='')
-print()
-
-for color in sorted(colors, reverse=True):
-    print('{} '.format(color), end='')
-print()
-
-for color in sorted(colors, key=len):
-    print('{} '.format(color), end='')
-print()
-
-def find(seq, target):
-    for i, value in enumerate(seq):
-        if value == target:
-            break
-    else:
-        return -1
-    return i
-
-print(find(range(10), 20))
-
-d = {'matthew': 'blue', 'rachel': 'green', 'raymond': 'red'}
-
-for k, v in d.items():
-    print(k, v)
-
-
-
+for i, data in enumerate(l, 0):
+    print(i, data)
