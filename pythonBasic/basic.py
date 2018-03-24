cd00653e65950ed5a4e95c4021f3ec43cf2e3bd3
leo <zhu_lia@Ctrip.com>
2018-03-24 18:01:05 +0800

    leodeMacBook-Pro-2.local Sat Mar 24 18:01:05 CST 2018

--- pythonBasic/basic.py
+++ pythonBasic/basic.py
@@ -1,44 +1,49 @@
-# l = [
-#     ['test', 'hello', 'world', 'sm', 'philpbin', 'english'],
-#     ['test2', 'hello2', 'world2', 'sm2', 'philpbin2', 'english2'],
-#     ]
-#
-# for i, data in enumerate(l, 0):
-#     print(i, data)
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # num_dict = {}
+        # for i, v in enumerate(nums):
+        #     rem = target - v
+        #     if rem in num_dict:
+        #         return [num_dict[rem], i]
+        #     num_dict[v] = i
+        #     print(num_dict)
+        # return None
 
-import numpy as np
+        num_list = []
+        for i, v in enumerate(nums):
+            rem = target - v
+            if rem in num_list:
+                return [num_list.index(rem), i]
+            num_list.append(v)
+        return None
 
-# data = np.loadtxt('linear_regression_data1.txt', delimiter=',')
-# print(data[:, 0])
-# print(np.c_[np.ones(data.shape[0]), data[:, 0]])
+s = Solution()
+print(s.twoSum([3, 5, 2, 4, 8, 1, 2, 777, 15], 6))
+# print(list(v for i, v in enumerate([3, 2, 4, 15]) if v < 6))
 
-# print(data[:,1])
-# print(np.c_[data[:,1]])
+# num_dict = {3: 0, 1: 1, 4: 2, 2: 3, -2: 4, 5: 5}
 
-a = np.ones([2, 3])
-# b = np.ones([2, 3])
-# print(a.size)
-
-a = np.array([[1, 2],
-              [3, 4]])
-b = np.array([[5], [6]])
-
-# print(a.dot(b))
-
-print(a)
-
-a = np.array([1, 2, 3, 4])
-b = np.array([5, 6, 7, 8])
-
-print(a.dot(b))
-
-
-# print(np.ones([2, 1]))
-# print(np.ones((2, 1)))
-
-# print(a.T.shape)
-#
-# m = np.matrix('[1, 2; 3, 4]')
-# print(m.T)
+l = [1, 5, 6, 2]
+if 5 in l:
+    print(l.index(5))
 
 
+# class Solution:
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         num_dict = {}
+#         for i, num in enumerate(nums):
+#             rem = target - num
+#             if rem in num_dict:
+#                 return [num_dict[rem], i]
+#             num_dict[num] = i
+#         return None
